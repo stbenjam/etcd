f2a7e0abfa429242eb469d349e643e523689f342
yoyinzyc <yoyinzyc@gmail.com>
2019-11-19 15:41:58 -0800

    mvcc: update to "etcd_debugging_mvcc_total_put_size_in_bytes"

--- mvcc/metrics.go
+++ mvcc/metrics.go
@@ -309,11 +309,11 @@ var (
 	reportCompactRevMu sync.RWMutex
 	reportCompactRev   = func() float64 { return 0 }
 
-	putSizeGauge = prometheus.NewGauge(
+	totalPutSizeGauge = prometheus.NewGauge(
 		prometheus.GaugeOpts{
-			Namespace: "etcd",
+			Namespace: "etcd_debugging",
 			Subsystem: "mvcc",
-			Name:      "put_size_in_bytes",
+			Name:      "total_put_size_in_bytes",
 			Help:      "The total size of put kv pairs seen by this member.",
 		})
 )
